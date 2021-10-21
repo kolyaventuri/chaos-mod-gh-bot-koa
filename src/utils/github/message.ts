642574feeafa74cc1fd7e1c7f62c88cffa7eefd5
Kolya Venturi <me@kolya.co>
2021-10-20 21:29:18 -0700

    Fix issue with missing reason

--- src/utils/github/message.ts
+++ src/utils/github/message.ts
@@ -1,11 +1,14 @@
 import {getString} from '../i18n';
 import {IssueDetails} from '../issue/types';
 
+const defaultString = getString('submission');
+
 export const getMessage = (details: IssueDetails): string => {
   const headerKey =
     details.problems.length > 1 ? 'messageBaseMultiple' : 'messageBase';
   const typeString = getString(`issueType.${details.type}`);
-  let result = `${getString(headerKey, {type: typeString})}\n`;
+  console.log(details);
+  let result = `${getString(headerKey, {type: typeString || defaultString})}\n`;
 
   const problemList = details.problems.map((problem) => {
     const key = `issues.${problem}`;
